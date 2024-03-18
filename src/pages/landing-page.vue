8f39ef3c4e3c871eec71659047bbd5b38f45814b
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-03-19 01:57:27 +0500

    services

--- src/pages/landing-page.vue
+++ src/pages/landing-page.vue
@@ -1,14 +1,11 @@
 <template>
    <Header />
    <KnowUs />
+   <Services />
 </template>
 
 <script setup>
 import Header from "../components/header.vue"
 import KnowUs from "../components/knows-us.vue"
-
-
-
-
-
+import Services from "../components/our-services.vue"
 </script>
