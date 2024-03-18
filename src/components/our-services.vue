2eac1997a4fe63bba03bdb5fc1e93d3660d50e03
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-03-19 02:02:48 +0500

    services

--- src/components/our-services.vue
+++ src/components/our-services.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container-fluid bg-white px-0  ">
+    <div class="container-fluid bg-services px-0  ">
         <div class="container  py-5 px-3 ">
             <div class="row ">
                 <div class="col-12  my-4 ">
