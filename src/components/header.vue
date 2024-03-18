5ae127a1a7436d26c641f75cc6c017ebd0dff90d
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-03-19 00:02:22 +0500

    nav

--- src/components/header.vue
+++ src/components/header.vue
@@ -18,7 +18,6 @@
             </div>
         </div>
     </div>
-
 </template>
 <script setup>
 import "@/src/assets/scss/components/_header.scss"
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,9 +1,9 @@
 <template>
-    <nav class="navbar  navbar-expand-lg  m-0 w-100 bg-primary mb-2 mb-md-5">
+    <nav class="navbar  navbar-expand-lg  m-0 w-100 bg-transparent mb-2 mb-md-5">
         <div class="container px-3 px-sm-0 w-100 align-items-center">
             <a class="navbar-brand " href="#">
-                <h3 class="text-white mb-0 pb-0">BucketList</h3>
-                <h6 class="text-white mb-0 pb-0">Explore</h6>
+                <img src="../assets/images/nav/logo.png" width="auto" height="auto" alt="logo"
+                    class="object-fit-contain ">
             </a> <button class="navbar-toggler border-0 p-1 navbar-button" type="button" data-bs-toggle="offcanvas"
                 data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-list"
@@ -18,23 +18,50 @@
                     <button type="button" class="btn-close " data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1  ">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-pinterest"></a>
+
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pt-2 ">
                         <li class="nav-item mx-0 mx-lg-2">
                             <a class="nav-link   " href="#">Home</a>
                         </li>
                         <li class="nav-item mx-0 mx-lg-2">
-                            <a class="nav-link  " href="#">Features</a>
+                            <a class="nav-link  " href="#">About</a>
                         </li>
                         <li class="nav-item mx-0 mx-lg-2">
-                            <a class="nav-link  " href="#">Customize Toure</a>
+                            <a class="nav-link  " href="#">Services</a>
                         </li>
                         <li class="nav-item mx-0 mx-lg-2">
-                            <a class="nav-link  " href="#">About Us</a>
+                            <a class="nav-link  " href="#">Project</a>
                         </li>
                         <li class="nav-item mx-0 mx-lg-2">
-                            <a class="nav-link  " href="#">Contact Us</a>
+                            <a class="nav-link  " href="#">News</a>
+                        </li>
+                        <li class="nav-item mx-0 mx-lg-2">
+                            <a class="nav-link  " href="#">Shop</a>
+                        </li>
+                        <li class="nav-item mx-0 mx-lg-2">
+                            <a class="nav-link  " href="#">Contact </a>
+                        </li>
+                        <li class="nav-item mx-0 mx-lg-2 d-none-contact">
+
+                            <img src="../assets/images/nav/search.png" alt="search" width="auto" height="auto"
+                                class="object-fit-contain float-start  pt-2">
+                        </li>
+                        <li class="nav-item mx-0 mx-lg-2 d-none-contact">
+
+                            <img src="../assets/images/nav/cart.png" alt="search" width="auto" height="auto"
+                                class="object-fit-contain float-start  pt-2">
+                        </li>
+                        <li class="nav-item mx-0 mx-lg-2 d-none-contact">
+
+                            <img src="../assets/images/nav/contact.png" alt="search" width="auto" height="auto"
+                                class="object-fit-contain float-start ">
                         </li>
                     </ul>
+
                 </div>
             </div>
         </div>
