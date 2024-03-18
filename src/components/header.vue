41e1e9d261759a3e846e75eca3ec3db2c335cff9
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-03-18 22:53:22 +0500

    header

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,22 +2,23 @@
     <div class="container-fluid bg-header px-0  ">
         <Nav />
         <div class="container  py-5 px-3 px-sm-0">
-            <div class="row">
-<div class="col-12 text-center">
-    <h1 class="w-100 text-white">
-        Natural Products
-    </h1>
-    <h1 class="w-100 text-white">
-        For Lovers of <span class="text-primary">Healthy</span> 
-            </h1>
-    <h1 class="w-100 text-white">
-        Organic Food
-            </h1>
-</div>
+            <div class="row mb-5">
+                <div class="col-12 text-center">
+                    <h1 class="w-100 text-white">
+                        Natural Products
+                    </h1>
+                    <h1 class="w-100 text-white">
+                        For Lovers of <span class="text-primary">Healthy</span>
+                    </h1>
+                    <h1 class="w-100 text-white">
+                        Organic Food
+                    </h1>
+                    <a class="btn bg-secondary px-3 py-2 mt-3 rounded text-white">Discover More</a>
+                </div>
             </div>
         </div>
     </div>
-    
+
 </template>
 <script setup>
 import "@/src/assets/scss/components/_header.scss"
