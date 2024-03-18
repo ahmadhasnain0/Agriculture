119820d0803e2340eca7a35252e6abc42248ce3f
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-03-19 01:03:21 +0500

    know us

--- src/components/knows-us.vue
+++ src/components/knows-us.vue
@@ -1,6 +1,36 @@
 <template>
-    <div class="container-fluid bg-header px-0  ">
-        <div class="container  py-5 px-3 px-sm-0">
+    <div class="container-fluid bg-white px-0  ">
+        <div class="container  py-5 px-3 ">
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <p class="text-primary mb-1 ">Get to know us</p>
+                    <h1>Leader in Agriculture
+                        Market</h1>
+                    <p class="f-14 text-light-gray">There are many variations of passages of Lorem Ipsum available
+                        majority have suffered alteration in some form, by injected humour,
+                        or randomised words which don’t look even slightly believable</p>
+                       <div class="row mt-5">
+                        <div class="col-12 col-sm-6 pe-0 pe-lg-5" >
+                            <p class="text-secondary ">
+                                Modern agriculture types
+                            </p>
+                            <p class="f-14 text-light-gray">
+                                We're here for you from start
+to finish  </p>
+<a class="btn bg-secondary px-4 py-3 mt-3 rounded text-white f-12">Discover More</a>
+
+                        </div>
+                        <div class="col-12 col-sm-6 mt-3 mt-sm-0 d-flex justify-content-center" >
+                            <img src="../assets/images/know-us/modren.png" alt="modren" width="auto" height="auto" class="object-fit-contain">
+                        </div>
+
+                        </div> 
+                </div>
+                <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+                    <img src="../assets/images/know-us/pic.PNG" alt="modren" width="auto" height="auto" class="object-fit-contain">
+
+                </div>
             </div>
-            </div>
+        </div>
+    </div>
 </template>
--- src/pages/landing-page.vue
+++ src/pages/landing-page.vue
@@ -1,10 +1,11 @@
 <template>
    <Header />
-   
+   <KnowUs />
 </template>
 
 <script setup>
 import Header from "../components/header.vue"
+import KnowUs from "../components/knows-us.vue"
 
 
 
