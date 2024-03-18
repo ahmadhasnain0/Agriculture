07562ad4630ae170445015c58f13468d69bcb37c
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-03-19 02:03:36 +0500

    services

--- src/components/our-services.vue
+++ src/components/our-services.vue
@@ -6,54 +6,53 @@
                     <p class="text-primary mb-1 text-center ">Our Services</p>
                     <h1 class="text-center mb-5">What We Offer</h1>
                     <div class="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center">
-  <div class="col">
-    <div class="card h-100">
-      <img src="../assets/images/services/pic1.png" class="card-img-top"
-        alt="sercices1" />
-        <img src="../assets/images/services/icon1.png" width="80px" height="80px" class="object-fit-contain float-end d-flex align-self-end card-icon" 
-        alt="sercices1" />
-      <div class="card-body px-4">
-        <h5 class="card-title">Agriculture Products</h5>
-        <p class="card-text text-light-gray f-14">
-            Lorem ipsum is simply free available.
-Aenean leo quam. Pellentesque
-semornare vestibulum.
-        </p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="../assets/images/services/pic2.png" class="card-img-top"
-        alt="Palm Springs Road" />
-        <img src="../assets/images/services/icon2.png" width="80px" height="80px" class="object-fit-contain float-end d-flex align-self-end card-icon" >
-
-      <div class="card-body px-4">
-        <h5 class="card-title">Organic Products</h5>
-        <p class="card-text text-light-gray f-14">
-            Lorem ipsum is simply free available.
-Aenean leo quam. Pellentesque
-semornare vestibulum.
-        </p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img src="../assets/images/services/pic3.png" class="card-img-top"
-        alt="Los Angeles Skyscrapers" />
-        <img src="../assets/images/services/icon3.png" width="80px" height="80px" class="object-fit-contain float-end d-flex align-self-end card-icon" >
-
-      <div class="card-body px-4">
-        <h5 class="card-title">Dairy Products</h5>
-        <p class="card-text text-light-gray f-14">Lorem ipsum is simply free available.
-Aenean leo quam. Pellentesque
-semornare vestibulum.</p>
-      </div>
-    </div>
-  </div>
-  
-</div>
+                        <div class="col">
+                            <div class="card h-100">
+                                <img src="../assets/images/services/pic1.png" class="card-img-top" alt="sercices1" />
+                                <img src="../assets/images/services/icon1.png" width="80px" height="80px"
+                                    class="object-fit-contain float-end d-flex align-self-end card-icon"
+                                    alt="sercices1" />
+                                <div class="card-body px-4">
+                                    <h5 class="card-title">Agriculture Products</h5>
+                                    <p class="card-text text-light-gray f-14">
+                                        Lorem ipsum is simply free available.
+                                        Aenean leo quam. Pellentesque
+                                        semornare vestibulum.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card h-100">
+                                <img src="../assets/images/services/pic2.png" class="card-img-top"
+                                    alt="Palm Springs Road" />
+                                <img src="../assets/images/services/icon2.png" width="80px" height="80px"
+                                    class="object-fit-contain float-end d-flex align-self-end card-icon">
+                                <div class="card-body px-4">
+                                    <h5 class="card-title">Organic Products</h5>
+                                    <p class="card-text text-light-gray f-14">
+                                        Lorem ipsum is simply free available.
+                                        Aenean leo quam. Pellentesque
+                                        semornare vestibulum.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card h-100">
+                                <img src="../assets/images/services/pic3.png" class="card-img-top"
+                                    alt="Los Angeles Skyscrapers" />
+                                <img src="../assets/images/services/icon3.png" width="80px" height="80px"
+                                    class="object-fit-contain float-end d-flex align-self-end card-icon">
+                                <div class="card-body px-4">
+                                    <h5 class="card-title">Dairy Products</h5>
+                                    <p class="card-text text-light-gray f-14">Lorem ipsum is simply free available.
+                                        Aenean leo quam. Pellentesque
+                                        semornare vestibulum.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
