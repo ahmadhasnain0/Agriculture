3fd776fce93452553b23918cd74e277e7ab536b7
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-03-28 19:00:52 +0500

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,21 +11,5 @@ export default {
     prefix: false
   },
 
-  // Additional build configuration if needed
-  build: {
-    // Extend webpack config to handle Bootstrap JavaScript if necessary
-    extend(config, ctx) {
-      // Example: Add Bootstrap JavaScript support
-      config.module.rules.push({
-        test: /\.js$/,
-        include: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }]
-      });
-    }
-  }
+ 
 }
