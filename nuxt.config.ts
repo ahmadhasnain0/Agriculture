796f2f6465e7f7704e90dd73925687fc4ca63069
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-03-28 13:36:41 +0500

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,19 +7,5 @@ export default {
     { src: '~/plugins/bootstrap.js', mode: 'client' }
   ],
 
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {
-      // Add rules for processing images
-      config.module.rules.push({
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      });
-    },
-  },
+
 }
